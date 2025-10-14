8f0cb86f161274723c440cae66e06cf7b0d351f4
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-14 16:03:36 +0900

    🐛 fix: admin page

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,1 +1,22 @@
+from django.contrib import admin
+from .models import Users
+
 # Register your models here.
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "nickname",
+        "name",
+        "phone_number",
+        "is_admin",
+        "is_staff",
+        "is_active",
+        "last_login",
+    )
+
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email", "nickname", "phone_number")
+    readonly_fields = ("is_admin", "created_at", "updated_at", "last_login")
+    ordering = ("-created_at",)
