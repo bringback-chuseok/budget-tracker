4df62f7308481e5a704aca0ee888a0bb3237f66c
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-16 13:01:29 +0900

    📝 Uri / to none
    '/'은 권장되지 않는 uri 패턴이므로 ''로 수정

--- app/histories/urls.py
+++ app/histories/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "histories"
 
 urlpatterns = [
-    path("/", views.HistoryListView.as_view(), name="list"),
+    path("", views.HistoryListView.as_view(), name="list"),
     path("create/", views.HistoryCreateView.as_view(), name="create"),
     path("<int:pk>/", views.HistoryDetailView.as_view(), name="detail"),
     path("<int:pk>/update/", views.HistoryUpdateView.as_view(), name="update"),
