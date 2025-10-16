b7301cd1373da4be7524921ab0aa435ca4a155bc
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-16 10:42:47 +0900

    test pre-commit

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/users/', include('app.users.urls')),
+    path("api/users/", include("app.users.urls")),
+    path("histories/", include("app.histories.urls")),
 ]
