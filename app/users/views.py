844445caaeab04ee5f304a60af229c8e46e3c89d
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-16 10:54:13 +0900

    ✨ Run pre-commit
    ruff & black check completed

--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Users
 from .serializers import UserSerializer
+
 
 class UserListView(APIView):
     def get(self, request):
