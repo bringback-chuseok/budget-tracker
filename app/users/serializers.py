cc4aaa4c958f163c12665120225f049a269844cd
TeaCat-Neya <TeaCat0416@gmail.com>
2025-10-16 10:54:13 +0900

    ✨ Run pre-commit
    ruff & black check completed

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+
 from .models import Users  # 모델 예시
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ['id', 'email', 'nickname']
+        fields = ["id", "email", "nickname"]
--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from .views import UserListView
 
 urlpatterns = [
-    path('', UserListView.as_view(), name='user-list'),
+    path("", UserListView.as_view(), name="user-list"),
 ]
