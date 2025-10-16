f173a83c1a12d2c5fd0e3c572042f9e761fdf7a6
BH13KDR <bh13kdr@example.com>
2025-10-16 16:26:54 +0900

    📦 accountapp 모듈 추가

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -5,6 +5,15 @@ from app.users.models import Users
 
 # Create your models here.
 class Accounts(models.Model):
+    #계좌타입. 저축.입출금.업무.투자.대출.
+    ACCOUNT_TYPES = [
+        ('SAVINGS', 'Savings'),
+        ('CHECKING', 'Checking'),
+        ('BUSINESS', 'Business'),
+        ('INVESTMENT', 'Investment'),
+        ('LOAN', 'Loan'),
+    ]
+
     user_id = models.ForeignKey(
         Users, on_delete=models.CASCADE, null=False, related_name="accounts"
     )
@@ -14,6 +23,10 @@ class Accounts(models.Model):
     bank_code = models.CharField(max_length=20)
     account_type = models.CharField(max_length=20)
     balance = models.DecimalField(decimal_places=2, max_digits=20)
+    #+삭제,생성,수정
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.account_name} - {self.account_type}"
